f9433792e3b005ae7b1b760416a0401bb3f56471
mit.levkin@vk.com <mit.levkin@vk.com>
2025-06-29 20:02:13 +0300

    init commit with routing

---
+++ app/profile.tsx
@@ -0,0 +1,8 @@
+import { Text, View } from "react-native";
+export default function Profile() {
+  return (
+    <View className=" flex  items-center justify-center p-4">
+      <Text className="text-red-600">Profile</Text>;
+    </View>
+  );
+}
